bd28238d301a4b660a07d66540d0d4fdf127a445
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2021-07-13 15:35:04 -0500

    ajuste

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -11,6 +11,7 @@ router.get('/empleados', (req, res)=>{
         }
     });
 });
+
 router.get('/empleados/:id',(req, res)=>{
     const { id } = req.params;
     mysqlconection.query('SELECT * FROM empleado WHERE id_empleado=?',[id],(err, rows, fields)=>{
@@ -22,4 +23,28 @@ router.get('/empleados/:id',(req, res)=>{
     })
 });
 
+router.post('/create', function(req, res, next) {
+    var product_name = req.body.product_name;
+    var sku = req.body.sku;
+    var price = req.body.price;
+    var sql = `INSERT INTO products (product_name, sku, price, created_at) VALUES ("${product_name}", "${sku}", "${price}", NOW())`;
+    db.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log('record inserted');
+    res.redirect('/products');
+    });
+});
+
+router.post('/edit/:id', function(req, res, next) {
+    var product_name = req.body.product_name;
+    var sku = req.body.sku;
+    var price = req.body.price;
+    var id = req.params.id;
+    var sql = `UPDATE products SET product_name="${product_name}", sku="${sku}", price="${price}" WHERE id=${id}`;
+    db.query(sql, function(err, result) {
+    if (err) throw err;
+    console.log('record updated!');
+    res.redirect('/products');
+    });
+});
 module.exports = router;

8f7043c93daccd5e0da23fc83be06df8c5c22e31
Pedro Cruz <cruzrodriguezpedroj@gmail.com>
2021-08-03 15:28:29 -0500

    cambios

--- src/routes/employees.js
+++ src/routes/employees.js
@@ -35,16 +35,29 @@ router.post('/create', function(req, res, next) {
     });
 });
 
-router.post('/edit/:id', function(req, res, next) {
-    var product_name = req.body.product_name;
-    var sku = req.body.sku;
-    var price = req.body.price;
-    var id = req.params.id;
-    var sql = `UPDATE products SET product_name="${product_name}", sku="${sku}", price="${price}" WHERE id=${id}`;
-    db.query(sql, function(err, result) {
-    if (err) throw err;
-    console.log('record updated!');
-    res.redirect('/products');
-    });
+router.put('/empleados/edit/:id', function(req, res, next) {
+
+
+    let {id} =  req.params
+    let {nombre, aPaterno} =   req.body;
+
+    mysqlconection.query('UPDATE empleado SET nombre = ?, ap_paterno = ? WHERE id_empleado = ?', [nombre, aPaterno, id], function (error, results, fields) {
+        if (error) throw error;
+      
+
+        console.log(results)
+
+      });
+
+    // var product_name = req.body.product_name;
+    // var sku = req.body.sku;
+    // var price = req.body.price;
+    // var id = req.params.id;
+    // var sql = `UPDATE products SET product_name="${product_name}", sku="${sku}", price="${price}" WHERE id=${id}`;
+    // db.query(sql, function(err, result) {
+    // if (err) throw err;
+    // console.log('record updated!');
+    // res.redirect('/products');
+    // });
 });
 module.exports = router;
